e7dcadc4629d75e83212d6afff31fc89501e0c60
Leonardo Razovic <razovic.1712242@studenti.uniroma1.it>
2022-07-31 22:49:15 +0100

    Test common cases

--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@ use crate::{mock::*, Error};
 use frame_support::{assert_noop, assert_ok};
 
 #[test]
-fn stake_works_for_simple_value() {
+fn stake_works() {
     new_test_ext().execute_with(|| {
         // Dispatch a signed extrinsic.
         assert_ok!(TemplateModule::stake(Origin::signed(1), 42));
@@ -10,12 +10,31 @@ fn stake_works_for_simple_value() {
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn stake_too_much_tokens() {
     new_test_ext().execute_with(|| {
-        // Ensure the expected error is thrown when no value is present.
-        // assert_noop!(
-        //     TemplateModule::cause_error(Origin::signed(1)),
-        //     Error::<Test>::NoneValue
-        // );
+        // Ensure the expected error is thrown when you unstake more than you have.
+        assert_noop!(
+            TemplateModule::stake(Origin::signed(1), 1024),
+            Error::<Test>::NotEnoughMainToken
+        );
+    });
+}
+
+#[test]
+fn unstake_works() {
+    new_test_ext().execute_with(|| {
+        // Dispatch a signed extrinsic.
+        assert_ok!(TemplateModule::unstake(Origin::signed(1), 42));
+    });
+}
+
+#[test]
+fn unstake_too_much_tokens() {
+    new_test_ext().execute_with(|| {
+        // Ensure the expected error is thrown when you unstake more than you have.
+        assert_noop!(
+            TemplateModule::unstake(Origin::signed(1), 1024),
+            Error::<Test>::NotEnoughStakedToken
+        );
     });
 }
